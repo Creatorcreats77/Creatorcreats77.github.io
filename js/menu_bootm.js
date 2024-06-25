f8903f78db5ffea66b2c67481b5591e08b88a825
MarsvenOvenars <marsvenovenars@gmail.com>
2024-06-25 04:31:39 -0700

    7.13

--- js/menu_bootm.js
+++ js/menu_bootm.js
@@ -1,3 +1,21 @@
+function Yza_gaytmak(){
+    wide_projuct_place = document.getElementById("widely_product")
+    wide_projuct_place.innerHTML ="";
+    widely_product_content = document.getElementById("widely_product_content")
+    if(products_main_first.classList.contains("open")){
+        window.location.href = "./index.html"
+    }
+    else{
+        products_main_first.classList.remove("hide")
+        products_main_first.classList.add("open")
+    product_widely = document.getElementById("product_widely")
+        product_widely.classList.remove("open")
+        product_widely.classList.add("hide")
+        swiperr()
+    }
+        
+}
+
 
         const list = document.querySelectorAll('.list');
         function activeLink(){
@@ -14,18 +32,22 @@ for (let i = 0; i < lists.length; i++){
     lists[i].addEventListener("click", function(){
         if (i==0){
             calculate_screenBottom(1)
+            
         }
         if (i==1){
             calculate_screenBottom(3)
+            
         }
         if (i==2){
             calculate_screenBottom(5)
+            // window.location.href= "index.html"
         }
         if (i==3){
             calculate_screenBottom(7)
         }
         if (i==4){
             calculate_screenBottom(9)
+            // window.location.href= "index.html"
         }
     })
 
@@ -43,4 +65,4 @@ function calculate_screenBottom(a){
     let indicator = document.getElementById("indicator");
     indicator.style.transform =`translateX(${(d)}px)`
 }
-calculate_screenBottom(1)
+calculate_screenBottom(5)
